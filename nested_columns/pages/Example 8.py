a74855a328a06837c8af55142948ca4f2db464f6
Johannes Rieke <johannes.rieke@gmail.com>
2022-11-25 02:08:12 +0100

    Add example 8 stub

---
+++ nested_columns/pages/Example 8.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+st.set_page_config(layout="wide", initial_sidebar_state="collapsed")
+import streamlit_nested_layout
+
+"""
+# Example 8
+https://github.com/streamlit/streamlit/pull/5266
+
+TBD!
+
+---
+"""
