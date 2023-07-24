e101bebb6e1b97773d0dde4b7721022dee19ab9a
Johannes Rieke <johannes.rieke@gmail.com>
2023-07-24 15:22:19 +0200

    test

--- config-option-issue/streamlit_app.py
+++ config-option-issue/streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 """
 If you see the error details below, then client.showErrorDetails = true is not working.
-If you see the redacted error message, then it is working.
+If you see the redacted error message, then it is working. test
 """
 
 raise ValueError("This description shouldn't show up if client.showErrorDetails = true is set")
