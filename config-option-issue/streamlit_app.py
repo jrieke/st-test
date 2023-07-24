527add02bd0ea6dd225a77c4b01d353e1aa94a2f
Johannes Rieke <johannes.rieke@gmail.com>
2023-07-24 15:03:55 +0200

    add config option issue

---
+++ config-option-issue/streamlit_app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+"""
+If you see the error details below, then client.showErrorDetails = true is not working.
+If you see the redacted error message, then it is working.
+"""
+
+raise ValueError("This description shouldn't show up if client.showErrorDetails = true is set")
